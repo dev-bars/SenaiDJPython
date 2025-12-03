6efe86cbad9467b64e641dd0fbee8b9a527b5da4
dev-bars <renan.br3@gmail.com>
2025-12-02 21:05:11 -0300

    commit - ajuste de receita para criar receitas

--- receitas/views.py
+++ receitas/views.py
@@ -6,7 +6,7 @@ from receitas.fabrica.receitasFake import gen_fake_receita
 a = "Senai"
 
 def home(request):
-    return render(request, "page/home.html", context={'nome': a, 'receita': [gen_fake_receita() for _ in range(3)]})
+    return render(request, "page/home.html", context={'nome': a, 'receitas': [gen_fake_receita() for _ in range(10)]})
 
 def receita(request):
     return render(request, 'page/receita-view.html', context={'nome': a,})
